0d37c7e265c92c589ba2d8d533f212d053e7dbda
Tom Hazledine <tom.hazledine@creation.agency>
2018-07-05 09:47:04 +0100

    2018 match data

---
+++ src/js/data/sixnations.js
@@ -0,0 +1,473 @@
+const sixNationsData = {
+    Y_2018: {
+        teams: ['ENG', 'FRA', 'IRE', 'ITA', 'SCO', 'WAL'],
+        matches: [
+            {
+                date: '20180203',
+                kick_off: '14:15 GMT (UTC+0)',
+                home: {
+                    team: 'WAL',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 34,
+                    tries: 4,
+                    conversions: 4,
+                    penalties: 2,
+                    drop: 0
+                },
+                away: {
+                    team: 'SCO',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 7,
+                    tries: 1,
+                    conversions: 1,
+                    penalties: 0,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180203',
+                kick_off: '17:45 CET (UTC+1)',
+                home: {
+                    team: 'FRA',
+                    result: 'loss',
+                    bonus_points: 1,
+                    score: 13,
+                    tries: 1,
+                    conversions: 1,
+                    penalties: 2,
+                    drop: 0
+                },
+                away: {
+                    team: 'IRE',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 15,
+                    tries: 0,
+                    conversions: 0,
+                    penalties: 4,
+                    drop: 1
+                }
+            },
+            {
+                date: '20180204',
+                kick_off: '16:00 CET (UTC+1)',
+                home: {
+                    team: 'ITA',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 15,
+                    tries: 2,
+                    conversions: 1,
+                    penalties: 1,
+                    drop: 0
+                },
+                away: {
+                    team: 'ENG',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 46,
+                    tries: 7,
+                    conversions: 4,
+                    penalties: 1,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180210',
+                kick_off: '14:15 GMT (UTC+0)',
+                home: {
+                    team: 'IRE',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 56,
+                    tries: 8,
+                    conversions: 8,
+                    penalties: 0,
+                    drop: 0
+                },
+                away: {
+                    team: 'ITA',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 19,
+                    tries: 3,
+                    conversions: 2,
+                    penalties: 0,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180210',
+                kick_off: '16:45 GMT (UTC+0)',
+                home: {
+                    team: 'ENG',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 12,
+                    tries: 2,
+                    conversions: 1,
+                    penalties: 0,
+                    drop: 0
+                },
+                away: {
+                    team: 'WAL',
+                    result: 'loss',
+                    bonus_points: 1,
+                    score: 6,
+                    tries: 0,
+                    conversions: 0,
+                    penalties: 2,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180211',
+                kick_off: '15:00 GMT (UTC+0)',
+                home: {
+                    team: 'SCO',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 32,
+                    tries: 2,
+                    conversions: 2,
+                    penalties: 6,
+                    drop: 0
+                },
+                away: {
+                    team: 'FRA',
+                    result: 'loss',
+                    bonus_points: 1,
+                    score: 26,
+                    tries: 2,
+                    conversions: 2,
+                    penalties: 4,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180223',
+                kick_off: '21:00 CET (UTC+1)',
+                home: {
+                    team: 'FRA',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 34,
+                    tries: 3,
+                    conversions: 2,
+                    penalties: 5,
+                    drop: 0
+                },
+                away: {
+                    team: 'ITA',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 17,
+                    tries: 2,
+                    conversions: 2,
+                    penalties: 1,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180224',
+                kick_off: '14:15 GMT (UTC+0)',
+                home: {
+                    team: 'IRE',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 37,
+                    tries: 5,
+                    conversions: 3,
+                    penalties: 2,
+                    drop: 0
+                },
+                away: {
+                    team: 'WAL',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 27,
+                    tries: 3,
+                    conversions: 3,
+                    penalties: 2,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180224',
+                kick_off: '16:45 GMT (UTC+0)',
+                home: {
+                    team: 'SCO',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 25,
+                    tries: 3,
+                    conversions: 2,
+                    penalties: 2,
+                    drop: 0
+                },
+                away: {
+                    team: 'ENG',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 13,
+                    tries: 1,
+                    conversions: 1,
+                    penalties: 2,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180310',
+                kick_off: '14:15 GMT (UTC+0)',
+                home: {
+                    team: 'IRE',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 28,
+                    tries: 4,
+                    conversions: 4,
+                    penalties: 0,
+                    drop: 0
+                },
+                away: {
+                    team: 'SCO',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 8,
+                    tries: 1,
+                    conversions: 0,
+                    penalties: 1,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180310',
+                kick_off: '17:45 CET (UTC+1)',
+                home: {
+                    team: 'FRA',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 22,
+                    tries: 1,
+                    conversions: 1,
+                    penalties: 5,
+                    drop: 0
+                },
+                away: {
+                    team: 'ENG',
+                    result: 'loss',
+                    bonus_points: 1,
+                    score: 16,
+                    tries: 1,
+                    conversions: 1,
+                    penalties: 3,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180311',
+                kick_off: '15:00 GMT (UTC+0)',
+                home: {
+                    team: 'WAL',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 38,
+                    tries: 5,
+                    conversions: 5,
+                    penalties: 1,
+                    drop: 0
+                },
+                away: {
+                    team: 'ITL',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 14,
+                    tries: 2,
+                    conversions: 2,
+                    penalties: 0,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180317',
+                kick_off: '13:30 CET (UTC+1)',
+                home: {
+                    team: 'ITL',
+                    result: 'loss',
+                    bonus_points: 1,
+                    score: 27,
+                    tries: 3,
+                    conversions: 3,
+                    penalties: 2,
+                    drop: 0
+                },
+                away: {
+                    team: 'SCO',
+                    result: 'win',
+                    bonus_points: 1,
+                    score: 29,
+                    tries: 4,
+                    conversions: 3,
+                    penalties: 1,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180317',
+                kick_off: '14:45 GMT (UTC+0)',
+                home: {
+                    team: 'ENG',
+                    result: 'loss',
+                    bonus_points: 0,
+                    score: 25,
+                    tries: 3,
+                    conversions: 0,
+                    penalties: 0,
+                    drop: 0
+                },
+                away: {
+                    team: 'IRE',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 24,
+                    tries: 3,
+                    conversions: 3,
+                    penalties: 1,
+                    drop: 0
+                }
+            },
+            {
+                date: '20180317',
+                kick_off: '17:00 GMT (UTC+0)',
+                home: {
+                    team: 'WAL',
+                    result: 'win',
+                    bonus_points: 0,
+                    score: 14,
+                    tries: 1,
+                    conversions: 0,
+                    penalties: 3,
+                    drop: 0
+                },
+                away: {
+                    team: 'FRA',
+                    result: 'loss',
+                    bonus_points: 1,
+                    score: 13,
+                    tries: 1,
+                    conversions: 1,
+                    penalties: 1,
+                    drop: 1
+                }
+            }
+            // {
+            //     match: 13,
+            //     date: '20180317',
+            //     home: 'WAL',
+            //     away: 'FRA',
+            //     home_score: 14,
+            //     away_score: 13
+            // },
+            // {
+            //     match: 12,
+            //     date: '20180311',
+            //     home: 'WAL',
+            //     away: 'ITA',
+            //     home_score: 38,
+            //     away_score: 14
+            // },
+            // {
+            //     match: 11,
+            //     date: '20180310',
+            //     home: 'IRE',
+            //     away: 'SCO',
+            //     home_score: 28,
+            //     away_score: 8
+            // },
+            // {
+            //     match: 10,
+            //     date: '20180310',
+            //     home: 'FRA',
+            //     away: 'ENG',
+            //     home_score: 22,
+            //     away_score: 16
+            // },
+            // {
+            //     match: 9,
+            //     date: '20180224',
+            //     home: 'IRE',
+            //     away: 'WAL',
+            //     home_score: 37,
+            //     away_score: 27
+            // },
+            // {
+            //     match: 8,
+            //     date: '20180224',
+            //     home: 'SCO',
+            //     away: 'ENG',
+            //     home_score: 25,
+            //     away_score: 13
+            // },
+            // {
+            //     match: 7,
+            //     date: '20180223',
+            //     home: 'FRA',
+            //     away: 'ITL',
+            //     home_score: 34,
+            //     away_score: 17
+            // },
+            // {
+            //     match: 6,
+            //     date: '20180211',
+            //     home: 'SCO',
+            //     away: 'FRA',
+            //     home_score: 32,
+            //     away_score: 26
+            // },
+            // {
+            //     match: 5,
+            //     date: '20180210',
+            //     home: 'IRE',
+            //     away: 'ITA',
+            //     home_score: 56,
+            //     away_score: 19
+            // },
+            // {
+            //     match: 4,
+            //     date: '20180210',
+            //     home: 'ENG',
+            //     away: 'WAL',
+            //     home_score: 12,
+            //     away_score: 6
+            // },
+            // {
+            //     match: 3,
+            //     date: '20180204',
+            //     home: 'ITL',
+            //     away: 'ENG',
+            //     home_score: 15,
+            //     away_score: 46
+            // },
+            // {
+            //     match: 2,
+            //     date: '20180203',
+            //     home: 'WAL',
+            //     away: 'SCO',
+            //     home_score: 34,
+            //     away_score: 7
+            // },
+            // {
+            //     match: 1,
+            //     date: '20180203',
+            //     home: 'FRA',
+            //     away: 'IRE',
+            //     home_score: 13,
+            //     away_score: 15
+            // }
+        ]
+    }
+};
+
+export default sixNationsData;
